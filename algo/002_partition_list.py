5e826bac0388be3b6d761e9a2c15db603f59462d
Saeed Hadikhanloo <saeed.hadikhanloo@hrflow.ai>
2026-02-06 17:46:21 +0100

    feat: add partition list exc

---
+++ algo/002_partition_list.py
@@ -0,0 +1,83 @@
+"""
+86. Partition List (LeetCode)
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+"""
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left = None
+        start_left = None
+
+        right = None
+        start_right = None
+
+        while head:
+            val = head.val
+            if val < x:
+                if not left:
+                    left = ListNode(val, None)
+                    start_left = left
+                else:
+                    left.next = ListNode(val, None)
+                    left = left.next
+
+            else:
+                if not right:
+                    right = ListNode(val, None)
+                    start_right = right
+                else:
+                    right.next = ListNode(val, None)
+                    right = right.next
+
+            head = head.next
+
+        if start_left and start_right:
+            if not left:
+                raise Exception(
+                    "Unexpected state: left is None but start_left is not None"
+                )
+
+            left.next = start_right
+            return start_left
+        if not start_left:
+            return start_right
+        if not start_right:
+            return start_left
+
+
+# Write an assert
+def list_to_linked_list(lst):
+    head = None
+    current = None
+    for val in lst:
+        if not head:
+            head = ListNode(val, None)
+            current = head
+        else:
+            current.next = ListNode(val, None)
+            current = current.next
+    return head
+
+
+def linked_list_to_list(head):
+    lst = []
+    while head:
+        lst.append(head.val)
+        head = head.next
+    return lst
+
+
+assert linked_list_to_list(
+    Solution().partition(list_to_linked_list([1, 4, 3, 2, 5, 2]), 3)
+) == [1, 2, 2, 4, 3, 5]
+print("All tests passed!")
